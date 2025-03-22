9241a1b0c5f00cf2a3f267a31e11238c5d4fb52a
se <73011125+sei65gorilla@users.noreply.github.com>
2025-03-22 22:50:48 +0900

    AllButton TouchableOpacity

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,4 +1,7 @@
-import { View, Text, StyleSheet, type ViewStyle } from "react-native"
+import {
+  TouchableOpacity, Text, StyleSheet,
+  type ViewStyle
+} from "react-native"
 
 interface Props {
   children: JSX.Element
@@ -8,9 +11,9 @@ interface Props {
 const CircleButton = (props: Props): JSX.Element => {
   const { children, style } = props
   return (
-    <View style={[styles.circleButton, style]}>
+    <TouchableOpacity style={[styles.circleButton, style]}>
       <Text style={styles.circleButtonLabel}>{children}</Text>
-    </View>
+    </TouchableOpacity>
   )
 }
 
